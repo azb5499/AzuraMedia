a35988cb9a9864743f341edd326d00cbd44c02a2
azb5499 <azb5499@gmail.com>
2025-02-08 16:09:20 +0200

    Added practice.html and moved practice.js into practice folder. Information about objects and prototypes added.

---
+++ practice/practice.js
@@ -0,0 +1,48 @@
+const car = {
+    name: "Toyota",
+    model: "Corolla",
+    carFullName : function() {
+        return this.name + " " + this.model;
+    },
+    company : "Toyota",
+    features : {
+        engine : "1300cc",
+        color : "white",
+        fuel : "petrol"
+    }
+};
+
+// console.log(car); 
+
+delete car.company;
+
+// console.log(car);
+// console.log(car.features);
+
+const features = Object.values(car.features);
+// console.log(features);
+
+let text = "";
+for (let [key, value] of Object.entries(car)) {
+    if (typeof value === 'object' && !Array.isArray(value)) {
+        text += key + ": " + JSON.stringify(value) + "<br>";
+    } else if (typeof value === 'function') {
+        text += key + ": " + value.call(car) + "<br>";
+    } else {
+        text += key + ": " + value + "<br>";
+    }
+}
+console.log(text);
+
+function Car(company,name,model,colour,wheelDrive = "front") {
+    this.company = company;
+    this.name = name;
+    this.model = model;
+    this.colour = colour;
+    this.wheelDrive = wheelDrive;
+}
+
+const car1 = new Car("Toyota","Corolla","2019","White");
+console.log(car1);
+
+Car.prototype.vin = "123456789";
