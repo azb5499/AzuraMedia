a4ef93119960d15234de6f484ccfcf83b7ddf120
azb5499 <azb5499@gmail.com>
2025-02-11 17:41:35 +0200

    Added basic server routing. Added the public folder. Added html files for home, add and view cars.

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const http = require('http');
+const fileSystem = require('fs');
+
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200; // Set the status code to 200 (OK)
-    res.setHeader('Content-Type', 'text/plain'); // Set the content type to plain text
-    res.end('Hello, World!\n'); // Send a response
+    if (req.url == "/") {
+        req.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(fileSystem.readFileSync('./public/index.html'));
+    } else if (req.url == "/add-cars") {
+        req.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(fileSystem.readFileSync('./public/add-cars.html'));
+    }
+    else if (req.url == "/view-cars") {
+        req.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(fileSystem.readFileSync('./public/add-cars.html'));
+    } else {
+        req.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end("<h1>404 Page Not Found</h1>");
+    };
 });
 
 const PORT = 3000;
